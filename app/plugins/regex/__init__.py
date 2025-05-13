b64ec234fad72512a12b258484d4b017ec9c8a9f
dendogg <densmaltz4@gmail.com>
2025-05-13 00:21:11 -0700

    Create __init__.py

---
+++ app/plugins/regex/__init__.py
@@ -0,0 +1,9 @@
+"""
+textMan | Regex Plugins Package
+----------------------------
+Contains plugins for regular expression operations.
+"""
+
+from app.plugins.regex.regex_plugin import RegexPlugin
+
+__all__ = ['RegexPlugin']
