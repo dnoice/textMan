bd750ffe0c1f6d7da8183b360db1bf879a4ebe6c
dendogg <densmaltz4@gmail.com>
2025-05-27 23:55:57 -0700

    Delete app directory

--- app/plugins/regex/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-textMan | Regex Plugins Package
-----------------------------
-Contains plugins for regular expression operations.
-"""
-
-from app.plugins.regex.regex_plugin import RegexPlugin
-
-__all__ = ['RegexPlugin']
